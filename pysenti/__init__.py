20ce2711e24c77c0052c0921c4ef1ca14f244aab
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-04-14 20:26:26 -0400

    [+] SentiResult data class

--- pysenti/__init__.py
+++ pysenti/__init__.py
@@ -1,4 +1,17 @@
 from __future__ import annotations
 
+from typing import NamedTuple, Iterable
 __version__ = '1.0.0'
+
+
+class SentiResult(NamedTuple):
+    positive: int
+    negative: int
+    neutral: int
+
+    def scale(self) -> int:
+        return self.positive - self.negative
+
+    def is_positive(self) -> bool:
+        return self.scale() > 0
 
