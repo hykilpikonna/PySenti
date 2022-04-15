1d72112aee014d058707e46fc86abd355758be90
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-04-14 20:26:03 -0400

    [+] Setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,39 @@
-import setuptools
+import pathlib
+import pysenti
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="sentistrength",
-    version="0.0.9",
-    author="Zhun Hung",
-    author_email="yongzhunhung@gmail.com",
-    description="Python 3 Wrapper for SentiStrength, reads a single or multiple input with options for binary class or scale output.",
-    long_description=long_description,
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="PySenti",
+    version=pysenti.__version__,
+    description="Python 3 Wrapper for SentiStrength",
+    long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/zhunhung/pysentistrength",
-    packages=setuptools.find_packages(),
+    url="https://github.com/hykilpikonna/PySenti",
+    author="Azalea Gui, Mike Thelwall",
+    author_email="me@hydev.org, m.thelwall@wlv.ac.uk",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
+    packages=['pysenti'],
+    package_data={'pysenti': ['pysenti/*']},
+    include_package_data=True,
+    install_requires=['setuptools', 'typing_extensions'],
+    # entry_points={
+    #     "console_scripts": [
+    #         "pysenti=pysenti.main:run",
+    #     ]
+    # },
 )
